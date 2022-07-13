c4371e502beea38e904f252ae4e7d1b7f2c42713
chajiuqqq <chajiuqqq@outlook.com>
2022-07-13 13:56:13 +0800

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, request
 import json
 
 app = Flask(__name__)
-redis = Redis(host='127.0.0.1', port=6379, decode_responses=True, charset="utf-8")
+redis = Redis(host='redis', port=6379, decode_responses=True, charset="utf-8")
 
 abnormal_ips_prefix = 'abnormal_IPs'
 banned_ips_prefix = 'banned_IPs'
